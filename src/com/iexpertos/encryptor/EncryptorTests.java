345aaf5376e53e887a494060afb91adfd8d654ae
Ruben Bernardez <rubenbp@gmail.com>
2014-03-02 00:54:47 +0100

    refactor: extract variable

--- src/com/iexpertos/encryptor/EncryptorTests.java
+++ src/com/iexpertos/encryptor/EncryptorTests.java
@@ -11,51 +11,51 @@ import org.junit.Test;
 
 public class EncryptorTests {
 	
-	Encryptor encryptor;
+	Encryptor letterEncryptor;
+	Encryptor numberEncryptor;
 	
 	@Before
 	public void setUp() {
-		encryptor = EncryptorFactory.createLetterEncryptor();
+		letterEncryptor = EncryptorFactory.createLetterEncryptor();
+		numberEncryptor = EncryptorFactory.createNumberEncryptor();
 	}
 
 	@Test
 	public void encripta_palabras() {
-		String encryptedWord = encryptor.cryptWord("hello");
+		String encryptedWord = letterEncryptor.cryptWord("hello");
 		assertThat(encryptedWord, is("jgnnq"));
 	}
 	
 	@Test(expected = InvalidParameterException.class)
 	public void no_permite_encriptar_palabras_con_espacios() {
-		encryptor.cryptWord("hello world");
+		letterEncryptor.cryptWord("hello world");
 	} 
 	
 	@Test
 	public void encripta_palabras_a_numeros() {
-		encryptor = EncryptorFactory.createNumberEncryptor();
-		String encryptedWord = encryptor.cryptWord("hello");
+		String encryptedWord = numberEncryptor.cryptWord("hello");
 		assertThat(encryptedWord, is("106103110110113"));
 	}
 	
 	@Test(expected = InvalidParameterException.class)
 	public void no_permite_encriptar_palabras_a_numeros_con_espacios() {
-		encryptor = EncryptorFactory.createNumberEncryptor();
-		encryptor.cryptWord("hello world");
+		numberEncryptor.cryptWord("hello world");
 	}
 	
 	@Test
 	public void encripta_ciertas_letras_de_palabras() {
-		String encryptedWord = encryptor.cryptWord("hello", "ho");
+		String encryptedWord = letterEncryptor.cryptWord("hello", "ho");
 		assertThat(encryptedWord, is("jellq"));
 	}
 	
 	@Test(expected = InvalidParameterException.class)
 	public void no_permite_encriptar_ciertas_letras_de_palabras_con_espacios() {
-		encryptor.cryptWord("hello world", "ho");
+		letterEncryptor.cryptWord("hello world", "ho");
 	}
 	
 	@Test
 	public void encripta_frases() {
-		String encryptedSentence = encryptor.cryptSentence("hello world");
+		String encryptedSentence = letterEncryptor.cryptSentence("hello world");
 		assertThat(encryptedSentence, is("jgnnq\"yqtnf"));
 	}
 }
